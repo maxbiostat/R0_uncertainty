1891805eaa8818bee37c63d70635eb5848a64043
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2015-08-14 23:27:03 +0100

    small fixes to script heading

--- code/gamma_ratio.R
+++ code/gamma_ratio.R
@@ -1,13 +1,14 @@
 ## Generates density values for a gamma ratio distribution which arises in
-## a certain problem of calculating the distribution of R0 in a specific SIR epidemic model 
+## a certain problem of calculating the distribution of R0 in a specific SIR epidemic model. 
 ## In fact, when N = 1, this is distribution is simply the distribution of the ratio of two gamma variates
 # Let R0 be given by \frac{\beta N}{\gamma}.
-# If \beta and \gamma are Gamma-distributed with # parameters {k1,t1} and {k2,t2}, respectively  and N
-# is a big positive constant, then R0  is distributed according to a gamma ratio distribution beta distribution of the second kind)
-# with parameters k1,t1,k2,t2 and N.
+# If \beta and \gamma are Gamma-distributed with
+# parameters {k1,t1} and {k2,t2}, respectively and N
+# is a big positive constant, then R0  is distributed according to a gamma ratio distribution or
+# beta distribution of the second kind) with parameters k1,t1,k2,t2 and N.
 # In this toy example, if we can represent our uncertainty about \beta and \gamma by means of Gamma random variables,
 # we can calculate the distribution and moments analytically.
-## Copyleft(or the one to blame): Carvalho, LMF (2014)
+## Copyleft(or the one to blame): Carvalho, LMF (2015)
 
 dgamma.ratio <- function(x, k1, k2, t1, t2, N){
   c <- {(N*t1*t2)^(k1+k2)}/(beta(k1,k2)*((N*t1)^k1)*(t2^k2)) # normalizing constant
