9612eac74889230be5e11eb5c9cbf47ad2e10d59
Luiz Max de Carvalho <lmcarvalho@ufrj.br>
2016-10-31 22:54:53 +0000

    various improvements; vectorisation; new optimiser for inverse cdf

--- code/gamma_ratio.R
+++ code/gamma_ratio.R
@@ -9,11 +9,18 @@
 # In this toy example, if we can represent our uncertainty about \beta and \gamma by means of Gamma random variables,
 # we can calculate the distribution and moments analytically.
 ## Copyleft(or the one to blame): Carvalho, LMF (2016)
-dgamma.ratio <- function(x, k1, k2, t1, t2, N){
-  c <- {(N*t1*t2)^(k1+k2)}/(beta(k1,k2)*((N*t1)^k1)*(t2^k2)) # normalising constant
-  d <- x^(k1-1) * ((t2*x + N*t1))^(-(k1+k2)) # density 
-  return(c * d)
+dgamma.ratio <- function(x, k1, k2, t1, t2, N, numstab = TRUE){
+  if(numstab){ ## "numerically stable" version
+    lc <- (k1+ k2)*(log(N) + log(t1) + log(t2))-{lbeta(k1, k2) + k1*(log(N) + log(t1)) + k2*log(t2)}
+    ld <- (k1-1)*log(x) - (k1+k2) * log(t2*x + N*t1)
+    return(exp(lc + ld))
+  }else{
+    c <- {(N*t1*t2)^(k1+k2)}/(beta(k1,k2)*((N*t1)^k1)*(t2^k2)) # normalising constant
+    d <- x^(k1-1) * ((t2*x + N*t1))^(-(k1+k2)) # density 
+    return(c * d)
+  }
 }
+dgamma.ratio <- Vectorize(dgamma.ratio)
 ##
 rgamma.ratio <- function(n, k1, k2, t1, t2, N){
   A <- rgamma(n = n, shape = k1, scale = t1)*N
@@ -24,17 +31,18 @@ rgamma.ratio <- function(n, k1, k2, t1, t2, N){
 pgamma.ratio <- function(q, k1, k2, t1, t2, N){
   integrate(dgamma.ratio, 0, q, k1 = k1, t1 = t1, k2 = k2, t2 = t2, N = N)$value
 }
+pgamma.ratio <- Vectorize(pgamma.ratio)
 ##
 qgamma.ratio <- function(p, k1, k2, t1, t2, N){# TODO: derive a proper upper limit (N doesn't work)
   optQuant <- function(x){
     (pgamma.ratio(x, k1 = k1, t1 = t1, k2 = k2, t2 = t2, N = N)-p)^2
   }
-  Upr <- (k1/(k2-1))*(t1*N/t2) + p*10 * sqrt( ((N*t1/t2)^2)* (k1*(k1+k2-1))/((k2-2)*(k2-1)^2))
-  Ans <- optimise(optQuant, lower = 0, upper = Upr)# this upper limit should work
-  return(Ans$minimum)
+  Ans <- nlminb(start = 1, objective = optQuant, lower = 0)
+  return(Ans$par)
 }
+qgamma.ratio <- Vectorize(qgamma.ratio)
 ##
-dgamma.ratio.Ncorrected<- function(x, k1, k2, t1, t2, N){
+dgamma.ratio.Ncorrected <- function(x, k1, k2, t1, t2, N){
   c <- {(N*t1*t2)^(k1+k2)}/(beta(k1, k2)*((N*t1)^k1)*(t2^k2)) 
   S <- integrate(dgamma.ratio, k1 = k1, k2 = k2, t1 = t1, t2 = t2, N = N, 0, N)$value
   cnew <- (1/S) * c # regularized normalising constant
@@ -47,6 +55,7 @@ entropy.gamma.ratio <- function(k1, t1, k2, t2, N){
                                                    k2 = k2, t2 = t2, N = N)}
   return(integrate(expectlog, 0, Inf)$value)
 }
+entropy.gamma.ratio <- Vectorize(entropy.gamma.ratio)
 # mean =  (k1/(k2-1))*(t1*N/t2)
 # mode = {(k1-1)*t1*N}/{t2*(k2+1)}
 # var = ((N*t1/t2)^2)* (k1*(k1+k2-1))/((k2-2)*(k2-1)^2)
