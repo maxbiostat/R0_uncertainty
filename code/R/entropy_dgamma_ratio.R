1d29a51ee19ce2581a25f0829c08a1a968f25cf8
Luiz Max Carvalho <lmcarvalho@ufrj.br>
2023-09-18 08:58:02 -0300

    MAJOR cleanup

--- code/R/entropy_dgamma_ratio.R
+++
@@ -1,5 +0,0 @@
-entropy.gamma.ratio <- function(k1, t1, k2, t2, N){
-  expectlog <- function(x){ -log(x) * dgamma.ratio(x, k1 = k1, t1 = t1,
-                                                   k2 = k2, t2 = t2, N = N)}
-  return(integrate(expectlog, 0, Inf)$value)
-}
